87ba08e12e4f1d8553c019dc1afc55140989e386
iqbalhossainraju <iqbalhossainraju@yahoo.com>
2022-04-13 10:59:34 +0600

    added on auth state change

--- src/hooks/usefirebase.js
+++ src/hooks/usefirebase.js
@@ -1,13 +1,50 @@
-import { useState } from 'react';
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { useEffect, useState } from 'react';
+import app from "../Firebase/firebase.init";
+
+
+const auth = getAuth(app);
+const GoogleProvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
 
     const signInWithGoogle = () => {
-        console.log('signed')
+        signInWithPopup(auth, GoogleProvider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+                setUser(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                console.log(error);
+            });
     }
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                // Sign-out successful.
+            })
+            .catch((error) => {
+                // An error happened.
+            });
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            setUser(user);
+        })
+    }, [])
+
+
     return {
         user,
+        handleSignOut,
         signInWithGoogle
     };
 };
